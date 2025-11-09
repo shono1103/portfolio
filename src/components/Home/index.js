86663eed31e926c69033f6cf09a86fa9a9abcd17
shonoshono <shono131103@gmail.com>
2025-11-09 09:59:46 +0900

    homeの表記を自分用に変更

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,62 +10,62 @@ import './index.scss'
 import styles from './index.scss'
 
 const Home = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
+	const [letterClass, setLetterClass] = useState('text-animate')
 
-  const nameArray = 'udip'.split('')
-  const jobArray = 'Software Engineer'.split('')
-  const interestArray = 'FullStack | DevOps '.split('')
+	const nameArray = 'hono'.split('')
+	const jobArray = 'Software Engineer'.split('')
+	const interestArray = 'Embeded | Infra | Web (a little)'.split('')
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
-    return () => clearTimeout(timer)
-  }, [])
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setLetterClass('text-animate-hover')
+		}, 4000)
+		return () => clearTimeout(timer)
+	}, [])
 
-  return (
-    <>
-      <div className="container home-page">
-        <div className="text-zone">
-          <h1>
-            <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
-            <br />
-            <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
-            <img src={LogoTitle} alt="Sudip Banerjee" />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={nameArray}
-              idx={15}
-            />
-            <br />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={jobArray}
-              idx={22}
-            />
-            <br />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={interestArray}
-              idx={22}
-            />
-          </h1>
-          <h2>
-            Streamlining Workflows | Deploying Intelligent Systems | Building
-            Scalable Applications
-          </h2>
-          <Link to="/contact" className="flat-button">
-            CONTACT ME
-          </Link>
-        </div>
-        <Logo />
-      </div>
+	return (
+		<>
+			<div className="container home-page">
+				<div className="text-zone">
+					<h1>
+						<span className={letterClass}>H</span>
+						<span className={`${letterClass} _12`}>i,</span>
+						<br />
+						<span className={`${letterClass} _13`}>I</span>
+						<span className={`${letterClass} _14`}>'m</span>
+						<img src={LogoTitle} alt="Sudip Banerjee" />
+						<AnimatedLetters
+							letterClass={letterClass}
+							strArray={nameArray}
+							idx={15}
+						/>
+						<br />
+						<AnimatedLetters
+							letterClass={letterClass}
+							strArray={jobArray}
+							idx={22}
+						/>
+						<br />
+						<AnimatedLetters
+							letterClass={letterClass}
+							strArray={interestArray}
+							idx={22}
+						/>
+					</h1>
+					<h2>
+						Streamlining Workflows | Deploying Intelligent Systems | Building
+						Scalable Applications
+					</h2>
+					<Link to="/contact" className="flat-button">
+						CONTACT ME
+					</Link>
+				</div>
+				<Logo />
+			</div>
 
-      <Loader type="pacman" />
-    </>
-  )
+			<Loader type="pacman" />
+		</>
+	)
 }
 
 export default Home
