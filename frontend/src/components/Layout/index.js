9e39f99394d705f50f5525e006b6642b4e28ea41
shono1103 <shono131103@gmail.com>
2026-01-10 16:39:25 +0000

    余白スクロールの原因であるstar要素を削除

--- frontend/src/components/Layout/index.js
+++ frontend/src/components/Layout/index.js
@@ -5,25 +5,17 @@ import Header from '../Header'
 import './index.scss'
 
 const Layout = () => {
-  return (
-    <div className="App">
-      <Header />
-      <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
+	return (
+		<div className="App">
+			<Header />
+			<div className="page">
+				<span className="tags top-tags">&lt;body&gt;</span>
 
-        <Outlet />
-        <Footer />
-        <span className="tags bottom-tags">
-          <div id="stars"></div>
-          <div id="stars2"></div>
-          <div id="stars3"></div>
-          &lt;/body&gt;
-          <br />
-          <span className="bottom-tag-html">&lt;/html&gt;</span>
-        </span>
-      </div>
-    </div>
-  )
+				<Outlet />
+				<Footer />
+			</div>
+		</div>
+	)
 }
 
 export default Layout
